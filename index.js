334db87eb7356e0000babd3ef7693fdb2305fb27
Jesus Morales <jesus.m@bnn.mx>
2020-07-21 15:18:04 -0500

    Mofigiying index,js

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-function comprobeDigits(dividendo, divisor) {
-  let result = dividendo % divisor
-  // console.log('El resultado es: ', result)
-  return result === 0
-}
+const utils = require('./utils')
+
+const { comprobeDigits, validate } = utils
+
+
 
 function printNumbers() {
+  console.log('New Version')
   for (let index = 1; index <= 100; index++) {
     let a = comprobeDigits(index, 5)
     let b = comprobeDigits(index, 3)
     let c = comprobeDigits(index, 15)
 
-    if (!a && !b && !c) {
-      console.log(index)
-    }
+    console.log(validate(a, b, c, index))
 
-    while (a && !b) {
-      console.log('Music')
-      a = false
-    }
-
-    while(b && !a) {
-      console.log('TI')
-      b = false
-    }
-
-    while (c) {
-      console.log('Musical')
-      c = false
-    }
   }
 }
 
