cba91dbd3f96f71d7908e212f8cb512c151948e3
Jesus Morales <jesus.m@bnn.mx>
2020-07-21 13:01:09 -0500

    First Solution

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-  function printNumbers() {
-    for (let index = 1; index <= 100; index++) {
+function comprobeDigits(dividendo, divisor) {
+  let result = dividendo % divisor
+  // console.log('El resultado es: ', result)
+  return result === 0
+}
+
+function printNumbers() {
+  for (let index = 1; index <= 100; index++) {
+    let a = comprobeDigits(index, 5)
+    let b = comprobeDigits(index, 3)
+    let c = comprobeDigits(index, 15)
+
+    if (!a && !b && !c) {
       console.log(index)
-      
+    }
+
+    while (a && !b) {
+      console.log('Music')
+      a = false
+    }
+
+    while(b && !a) {
+      console.log('TI')
+      b = false
+    }
+
+    while (c) {
+      console.log('Musical')
+      c = false
     }
   }
+}
 
-  printNumbers();
+printNumbers();
